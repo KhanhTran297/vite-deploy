d4332b2277d0982290db7bb4ae66165ec5a073c9
KhanhTran297 <giakhanh2972002@gmail.com>
2023-05-14 16:53:53 +0700

    modifiy viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/vite-deploy/",
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
+  // Hỗ trợ dấu @ trong đường dẫn + jsconfig.json
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
